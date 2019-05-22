10026ad40c9603649b5e3625c992e2c6c749e472
Guy Ross <guyallenross@gmail.com>
2019-05-21 23:44:42 -0400

    SearchSettingsType - created a type for web search settings.

---
+++ src/OpenWeb.Core/SearchSettingsType.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenWeb.Core
+{
+    public struct SearchSettingsType
+    {
+        public int MaxLinks;
+        public int MaxParagraphs;
+
+        public SearchSettingsType(int maxLinks, int maxParagraphs)
+        {
+            MaxLinks = maxLinks;
+            MaxParagraphs = maxParagraphs;
+        }
+    }
+}
