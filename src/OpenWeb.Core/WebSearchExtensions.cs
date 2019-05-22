b4e158dac74d8504c4a6c0584c022e764b3a8476
Guy Ross <guyallenross@gmail.com>
2019-05-21 23:47:32 -0400

    WebSearchExtensions -split up the select highest scored sentence extension into seperate functions. - added comments to the extensions.

--- src/OpenWeb.Core/WebSearchExtensions.cs
+++ src/OpenWeb.Core/WebSearchExtensions.cs
@@ -1,20 +1,52 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Collections.Generic;
 
 namespace OpenWeb.Core
 {
     public static class WebSearchExtensions
     {
-		public static string SelectHigestScoredSentence(this Dictionary<string, double> scoredStatements)
+        /// <summary>
+        /// Selects the head item from a set of paragraphs
+        /// </summary>
+        /// <param name="paragraphs"></param>
+        /// <returns></returns>
+        public static string SelectHead(this IEnumerable<KeyValuePair<string, double>> paragraphs)
         {
-            return scoredStatements
-                .OrderBy(statement => statement.Value)
-                .Reverse()
-                .First()
-                .Key;
+            return paragraphs
+                .Select(x => x.Key)
+				.First();
         }
 
-		public static double ScoreSentenceFromVectorizedKeywords(this string scoredStatement)
+        /// <summary>
+        /// Orders a set of paragraphs based on highest values.
+        /// </summary>
+        /// <param name="paragraphs"></param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<string, double>> OrderParagraphsByHighest(this Dictionary<string, double> paragraphs)
+        {
+            return paragraphs
+                .OrderBy(statement => statement.Value)
+                .Reverse();
+        }
+
+        /// <summary>
+        /// Orders a set of paragraphs based on lowest values.
+        /// </summary>
+        /// <param name="paragraphs"></param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<string, double>> OrderParagraphsByLowest(this Dictionary<string, double> paragraphs)
+        {
+            return paragraphs
+                .OrderBy(statement => statement.Value);                
+        }
+
+        /// <summary>
+        /// Generates a correctness score based on a set of vectorized keywords.
+        /// </summary>
+        /// <param name="scoredStatements"></param>
+        /// <param name="keywords"></param>
+        /// <returns></returns>
+		public static double ScoreFromVectorizedKeywords(this Dictionary<string, double> scoredStatements, IEnumerable<string> keywords)
         {
             throw new System.NotImplementedException();
         }
