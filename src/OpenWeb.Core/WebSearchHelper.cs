ceed41154f657aee07ca069574080febb5697c53
Guy Ross <guyallenross@gmail.com>
2019-05-22 00:27:19 -0400

    Moved out keyword creation to a help class.

---
+++ src/OpenWeb.Core/WebSearchHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace OpenWeb.Core
+{
+    public static class WebSearchHelper
+    {
+        public static List<string> SplitSenteceToKeywords(string sentence)
+        {
+            string cleanedQuestion = Regex.Replace(sentence , @"[^0-9A-Za-z ,]", "");
+
+            return cleanedQuestion
+                .Split(' ')
+                .ToList();
+        }
+    }
+}
